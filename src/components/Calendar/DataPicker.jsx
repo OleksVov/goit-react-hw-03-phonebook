3713752cd683769017682b5d09ede923a6938367
Oleksandr <alex.vovchok@gmail.com>
2023-08-03 14:26:06 +0300

    css

--- src/components/Calendar/DataPicker.jsx
+++ src/components/Calendar/DataPicker.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { useState } from "react";
 import Calendar from "react-calendar";
-// import 'react-calendar/dist/Calendar.css';
+import 'react-calendar/dist/Calendar.css';
 import './DataPicker.css';
+// import {formatDate} from "react-calendar/dist/cjs/index.js";
 
 
 
 const DataPicker = () => {
     const [date, setDate] = useState(new Date());
+    // const locales = 'en-Us';
+    // const options = {weekday: 'long'};
 
     const onChange = date => {
         setDate(date);
@@ -21,8 +24,10 @@ const DataPicker = () => {
             value={date}
             minDate={new Date()}
             locale="en-EN"
-            next2Label=""
-            prev2Label=""
+            // next2Label=""
+            // prev2Label=""
+            // formatShortWeekday={(locale, date) => formatDate(date, 'dd')}
+            // formatShortWeekday={(date) => new Intl.DateTimeFormat(locales, options).format(date)}
             />
         </div>
     )
